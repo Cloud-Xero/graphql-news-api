f3ef792c57cb2429af45850d0ab67ce36f7132aa
Cloud-Xero <hiropon201511@gmail.com>
2022-09-18 01:28:34 +0900

    first commit

---
+++ src/server.js
@@ -0,0 +1,22 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+// GraphQLスキーマの定義
+const typeDefs = gql`
+  type Query {
+    info: String!
+  }
+`;
+
+// リゾルバ関数
+const resolvers = {
+  Query: {
+    info: () => "HackerNews クローン",
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => console.log(`${url}でサーバーを起動中`));
