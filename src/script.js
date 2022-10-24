ad4ebb7272f00bc88fb1d975ec0b6c821aefda50
mercari-komamiya <tmp-h-komamiya@mercari.com>
2022-10-24 19:19:17 +0900

    operate DB with prisma client

---
+++ src/script.js
@@ -0,0 +1,24 @@
+// データベースにアクセスするためのクライアントライブラリ
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main(params) {
+  const newLink = await prisma.link.create({
+    data: {
+      description: "GraphQLチュートリアルを学ぶ",
+      url: "https://example.com",
+    },
+  });
+  const allLinks = await prisma.link.findMany();
+  console.log(allLinks);
+}
+
+main()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    // データベースを閉じる
+    prisma.$disconnect;
+  });
